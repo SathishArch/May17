7ee123f178520c00bfa6530a20ed9348df3d673f
SathishArch <sathishjoshva087@gmail.com>
2022-05-18 21:17:44 +0530

    Import Project

---
+++ src/org/test/Employeeinfo.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class Employeeinfo {
+	public static void main(String[] args) {
+		
+		
+		int clgId =105, Clgcode=1120;
+		if (clgId!=105 && Clgcode!=1123) {
+			System.out.println("Anna University");
+			
+		} else {
+			System.out.println("Other University");
+
+		}
+	}
+}
